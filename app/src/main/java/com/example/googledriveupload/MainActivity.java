5b9152db470497e8acb87a2afdda023ec5a57300
Yu-Tung <58674419+Yu-Tung@users.noreply.github.com>
2020-04-14 17:47:59 +0800

    從本地資料抓取檔案 修改檔案名稱&路徑

--- app/src/main/java/com/example/googledriveupload/MainActivity.java
+++ app/src/main/java/com/example/googledriveupload/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -29,67 +35,34 @@ import com.google.api.services.drive.DriveScopes;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     DriveServiceHelper driveServiceHelper;
     private final static String BR=System.getProperty("line.separator");
+    private Button btnChooseFile;
+    private static final int PICK_CSV_FROM_GALLERY_REQUEST_CODE = 100;
+    private String filePath;
+//    private  String[] filepa;
+    private List<String> filepa = new ArrayList<>();
+    private List<String> fileName = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        tvPath = findViewById(R.id.tvPath);
-//        btnChooseFile = findViewById(R.id.btnChooseFile);
-//        btnChooseFile.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//               aaa();
-//            }
-//        });
+        btnChooseFile = findViewById(R.id.btnChooseFile);
+        btnChooseFile.setOnClickListener(this);
 
         requestSignIn();
     }
 
 
-//    public void aaa(){
-//        FilenameFilter namefilter =new FilenameFilter(){
-//            private String[] filter={
-//                    "bmp","gif","jpg","png"
-//            };
-//            @Override
-//            public boolean accept(File dir, String filename){
-//                for(int i=0;i<filter.length;i++){
-//                    if(filename.indexOf(filter[i])!=-1)
-//                        return true;
-//                }
-//                return false;
-//            }
-//        };
-//        try{
-//            File filePath=new File(Environment.getExternalStorageDirectory()+"/");
-//            //String str=filePath.getName();取得檔案夾名稱
-//            //textFileName.setText(str);
-//            File[] fileList=filePath.listFiles(namefilter);
-//            CharSequence[] list =new CharSequence[fileList.length];
-//            for(int i=0;i<list.length;i++){
-//                list[i]=fileList[i].getName();
-//                tvPath.setText(tvPath.getText().toString()+list[i]+BR);
-//            }
-//
-//
-//        }catch(Exception e){
-//
-//        }
-//
-//    }@Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.activity_choose_file, menu);
-//        return true;
-//    }
 
     public void uploadFile(View view){
         ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
@@ -97,9 +70,13 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.setMessage("Please wait...");
         progressDialog.show();
 
-
-        String filePath = "/storage/emulated/0/aaaa.pdf";
-        driveServiceHelper.createFile(filePath)
+//        String filePath = "/storage/emulated/0/acx1.csv";
+//        driveServiceHelper.createFile(filePath)
+        int j = filepa.size();
+        for (int i =0;i<j;i++){
+           //沒資料時會一直繞圈
+        driveServiceHelper.fileName(fileName.get(i));
+        driveServiceHelper.createFile(filepa.get(i))
                 .addOnSuccessListener(new OnSuccessListener<String>() {
                     @Override
                     public void onSuccess(String s) {
@@ -115,13 +92,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+        filepa.clear();
+        fileName.clear();
+//        Log.d("aaa",""+fileName.size());
+    }
 
     public void requestSignIn(){
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .requestScopes(new Scope(DriveScopes.DRIVE_FILE),
                         new Scope(DriveScopes.DRIVE_APPDATA))
-                .requestIdToken("68625113567-abf38rjn2qdku3a5pirkoh9mjb6ks870.apps.googleusercontent.com")
                 .build();
 
         GoogleSignInClient client = GoogleSignIn.getClient(this,signInOptions);
@@ -129,49 +109,117 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(client.getSignInIntent(),400);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void handleSignInIntent(Intent data) {
+        GoogleSignIn.getSignedInAccountFromIntent(data)
+                .addOnSuccessListener(googleSignInAccount -> {
+                    GoogleAccountCredential credential = GoogleAccountCredential
+                            .usingOAuth2(MainActivity.this, Collections.singleton(DriveScopes.DRIVE_FILE));
 
-        switch (requestCode)
-        {
-            case 400:
-                if(resultCode == RESULT_OK) {
-                    handleSignInIntent(data);
-                }
+                    credential.setSelectedAccount(googleSignInAccount.getAccount());
+
+                    Drive googleDriveService =
+                            new Drive.Builder(
+                                    AndroidHttp.newCompatibleTransport(),
+                                    new GsonFactory(),
+                                    credential)
+                                    .setApplicationName("AppName")
+                                    .build();
+
+                    driveServiceHelper = new DriveServiceHelper(googleDriveService);
+
+                })
+                .addOnFailureListener(e -> e.printStackTrace());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnChooseFile:
+                chooseFile();
                 break;
-            default:
-                break;
+             default:
+                 break;
         }
     }
 
-    private void handleSignInIntent(Intent data) {
-        GoogleSignIn.getSignedInAccountFromIntent(data)
-                .addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
-                    @Override
-                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
-                        GoogleAccountCredential credential = GoogleAccountCredential
-                                .usingOAuth2(MainActivity.this, Collections.singleton(DriveScopes.DRIVE_FILE));
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode)
+        {
+            case 400:
+                if(resultCode == RESULT_OK ) {
+                    handleSignInIntent(data);
+                }
+                break;
 
-                        credential.setSelectedAccount(googleSignInAccount.getAccount());
-
-                        Drive googleDriveService =
-                                new Drive.Builder(
-                                        AndroidHttp.newCompatibleTransport(),
-                                        new GsonFactory(),
-                                        credential)
-                                        .setApplicationName("AppName")
-                                        .build();
-
-                        driveServiceHelper = new DriveServiceHelper(googleDriveService);
-
+            case PICK_CSV_FROM_GALLERY_REQUEST_CODE:
+                Uri selectedCsv =null;
+                ClipData clipData = null;
+                if (resultCode == Activity.RESULT_OK && data != null){
+                    selectedCsv = data.getData();
+                    if (clipData != null){
+                        catchFileInApp(selectedCsv,clipData);
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
+                    else if (Build.VERSION.SDK_INT>=16 && clipData== null){
+                        clipData = data.getClipData();
+                        catchFileInApp(selectedCsv,clipData);
                     }
-                });
+                }
+                    break;
+            default:
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void catchFileInApp(Uri selectedCsv,ClipData clipData){
+        if (selectedCsv != null){
+            String path = FileUtil.getFileAbsolutePath(this,selectedCsv);
+//            Log.d("path1",""+path);
+//            filePath = path;
+            filepa.add(path);
+            String name = FileUtil.fileName(path);
+            fileName.add(name);
+            Log.d("asd",fileName.get(0));
+            //設定檔案名稱
+        }else  if (clipData != null){
+            int count = clipData.getItemCount();
+            if (count > 0){
+                Uri[] uris = new Uri[count];
+                String[] paths = new String[count];
+                String[] names = new String[count];
+                for (int i=0;i<count;i++){
+                    uris[i] = clipData.getItemAt(i).getUri();
+                    paths[i] = FileUtil.getFileAbsolutePath(this,uris[i]);
+                    names[i] = FileUtil.fileName(paths[i]);
+                    Log.d("paee",paths[i]);
+                    filepa.add(paths[i]);
+                    fileName.add(names[i]);
+                }
+                Log.d("paee",fileName.get(1));
+                Log.d("paee",fileName.get(0));
+            }
+        }
+    }
+
+    private void chooseFile() {
+        //https://www.jianshu.com/p/c1656748849f mimeType類型
+        String mimeType = "text/comma-separated-values";
+        PackageManager packageManager = MainActivity.this.getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType(mimeType);
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent,PackageManager.MATCH_DEFAULT_ONLY);
+        if (list.size() > 0){
+            Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
+            picker.setType(mimeType);
+            picker.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+//            picker.addCategory(Intent.CATEGORY_OPENABLE);
+            picker.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+            Intent destIntent = Intent.createChooser(picker, "選取csv檔案");
+            startActivityForResult(destIntent,PICK_CSV_FROM_GALLERY_REQUEST_CODE);
+
+        }else{
+            Log.d("error","無可用的Activity");
+        }
     }
 }
